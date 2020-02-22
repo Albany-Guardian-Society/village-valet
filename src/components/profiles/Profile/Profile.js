4c034051e468c1d995d324368929f0eee18a5e62
MohaHayat <mohammadfhayat@gmail.com>
2020-02-22 18:05:11 -0500

    test commit

---
+++ src/components/profiles/Profile/Profile.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { connect } from "react-redux";
+
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import ProfileTable from "./Profile_Table.js";
+
+class Profile extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+    }
+
+    render() {
+        return (
+            <div>
+                <Row>
+                    Picture/Name
+                </Row>
+                <Row>
+                    <Col>
+                        <ProfileTable/>
+                    </Col>
+                </Row>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => ({
+});
+
+const mapDispatchToProps = dispatch => ({
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Profile);
---
+++ src/components/profiles/Profile/Profile_Table.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { connect } from "react-redux";
+import Button from 'react-bootstrap/Button'
+
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from 'react-bootstrap/Container'
+
+class PTable extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+    }
+
+    render() {
+        return (
+            <div>
+                <Container>
+                        <Row>
+                            <Button variant="primary" onClick={() => alert('ContactInfo')}>Contact Info</Button>
+                        </Row>
+                        <Row>
+                            <Button variant="primary">Address Log</Button>
+                        </Row>
+                        <Row>
+                            <Button variant="primary">Special Accommodations</Button>
+                        </Row>
+                        <Row>
+                            <Button variant="primary">Favorite Locations</Button>
+                        </Row>
+
+                        <Row>
+                            <Button variant="primary">Village Info</Button>
+                        </Row>
+                        <Row>
+                            <Button variant="primary">Preferred Drivers</Button>
+                        </Row>
+                </Container>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => ({
+});
+
+const mapDispatchToProps = dispatch => ({
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(PTable);
