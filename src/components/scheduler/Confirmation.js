8ce6e594f41072abfb4379471057975f773b00f8
Schmaj <jwschmalz@outlook.com>
2020-03-03 12:23:36 -0500

    Basic Confirmation Page

--- src/components/scheduler/Confirmation.js
+++ src/components/scheduler/Confirmation.js
@@ -2,6 +2,9 @@ import React, {Component} from "react";
 import {connect} from "react-redux";
 
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import MapContainer from "../google-maps/MapContainer";
 
 class Confirmation extends Component {
     constructor(props) {
@@ -18,7 +21,39 @@ class Confirmation extends Component {
         return (
             <Container className="Confirmation" style={{minWidth: "100%"}}>
                 <h1>Confirmation</h1>
+                <Row>
+                    <Col>
+                        <MapContainer>Trip Summary</MapContainer>
+                    </Col>
+                    <Col>
+                        <Row>
+                            Name:
+                        </Row>
+                        <Row>
+                            PICTURE
+                        </Row>
+                        <Row>
+                            Pickup:
+                        </Row>
+                        <Row>
+                            Dropoff:
+                        </Row>
+                        <Row>
+                            Trip Duration:
+                        </Row>
+                        <Row>
+                            Expected Traffic:
+                        </Row>
+                        <Row>
+                            Driver:
+                        </Row>
+                        <Row>
+                            Return Driver:
+                        </Row>
+                    </Col>
+                </Row>
             </Container>
+
         );
     }
 }
