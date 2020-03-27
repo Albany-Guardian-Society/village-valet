870173154be5487241712e5a8b9c4ad9d23339a7
jeremyettlinger <jeremy.ettlinger@gmail.com>
2020-03-27 13:17:09 -0400

    Now Confirmation page should work.

--- src/components/scheduler/Confirmation.js
+++ src/components/scheduler/Confirmation.js
@@ -20,30 +20,31 @@ class Confirmation extends Component {
 
     render() {
         return (
-            <Row>
-                <Col>
-                    <MapContainer>Trip Summary</MapContainer>
-                </Col>
-                <Col>
-                    <Row>Name: {`${this.props.active_ride.rider.first_name}`} {`${this.props.active_ride.rider.last_name}`}</Row>
-                    <Row>
-                        <Col>
-                            <Row>Pickup Location: {`${this.props.active_ride.locations.pickup.address}`}</Row>
-                            <Row>Dropoff Location: {`${this.props.active_ride.locations.dropoff.address}`}</Row>
-                            <Row>Return Location: {`${this.props.active_ride.locations.return.address}`}</Row>
-                        </Col>
-                        <Col>
-                            <Row>Pickup Time: {`${this.props.active_ride.locations.pickup.time}`}</Row>
-                            <Row>Dropoff Time: {`${this.props.active_ride.locations.pickup.time}`}</Row>
-                            <Row>Return Time: {`${this.props.active_ride.locations.pickup.time}`}</Row>
-                        </Col>
-                    </Row>
-                    <Row>Trip Duration: {`${this.props.active_ride.ride_data.time_total}`}</Row>
-                    <Row>Expected Traffic: {`${this.props.active_ride.ride_data.traffic}`}</Row>
-                    <Row>Driver: {`${this.props.active_ride.driver_1.first_name}`} {`${this.props.active_ride.driver_1.last_name}`}</Row>
-                    <Row>Return Driver: {`${this.props.active_ride.driver_1.first_name}`} {`${this.props.active_ride.driver_1.last_name}`}</Row>
-                </Col>
-            </Row>
+            <Container>
+                <Row>
+                    <Col>
+                        <MapContainer>Trip Summary</MapContainer>
+                    </Col>
+                    <Col>
+                        <Row>Name: {`${this.props.active_ride.rider.first_name}`} {`${this.props.active_ride.rider.last_name}`}</Row>
+                        <Row>
+                            <Col>
+                                <Row>Pickup Location: {`${this.props.active_ride.locations.pickup.address}`}</Row>
+                                <Row>Dropoff Location: {`${this.props.active_ride.locations.dropoff.address}`}</Row>
+                                <Row>Return Location: {`${this.props.active_ride.locations.return.address}`}</Row>
+                            </Col>
+                            <Col>
+                                <Row>Pickup Time: {`${this.props.active_ride.locations.pickup.time}`}</Row>
+                                <Row>Dropoff Time: {`${this.props.active_ride.locations.pickup.time}`}</Row>
+                                <Row>Return Time: {`${this.props.active_ride.locations.pickup.time}`}</Row>
+                            </Col>
+                        </Row>
+                        <Row>Trip Duration: {`${this.props.active_ride.ride_data.time_total}`}</Row>
+                        <Row>Expected Traffic: {`${this.props.active_ride.ride_data.traffic}`}</Row>
+                        <Row>Driver: {`${this.props.active_ride.driver_1.first_name}`} {`${this.props.active_ride.driver_1.last_name}`}</Row>
+                        <Row>Return Driver: {`${this.props.active_ride.driver_1.first_name}`} {`${this.props.active_ride.driver_1.last_name}`}</Row>
+                    </Col>
+                </Row>
             </Container>
         );
     }
