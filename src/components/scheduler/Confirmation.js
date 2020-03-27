75972f4b57caef9c9468e34d96299e589d7da2d8
jeremyettlinger <jeremy.ettlinger@gmail.com>
2020-03-27 13:14:56 -0400

    Made Confirmation page that reads info from previous parts of scheduler.  Still needs some formatting and Google Maps integration.

--- src/components/scheduler/Confirmation.js
+++ src/components/scheduler/Confirmation.js
@@ -4,6 +4,7 @@ import {connect} from "react-redux";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+
 import MapContainer from "../google-maps/MapContainer";
 
 class Confirmation extends Component {
@@ -19,47 +20,37 @@ class Confirmation extends Component {
 
     render() {
         return (
-            <Container className="Confirmation" style={{minWidth: "100%"}}>
-                <h1>Confirmation</h1>
-                <Row>
-                    <Col>
-                        <MapContainer>Trip Summary</MapContainer>
-                    </Col>
-                    <Col>
-                        <Row>
-                            Name: {this.props.active_ride.rider.first_name} {this.props.active_ride.rider.last_name}
-                        </Row>
-                        <Row>
-                            Pickup: {this.props.active_ride.locations.pickup}
-                        </Row>
-                        <Row>
-                            Dropoff: {this.props.active_ride.locations.dropoff}
-                        </Row>
-                        <Row>
-                            Trip Duration: {this.props.active_ride.ride_data.time_total}
-                        </Row>
-                        <Row>
-                            Expected Traffic: {this.props.active_ride.ride_data.traffic}
-                        </Row>
-                        <Row>
-                            Driver: {this.props.active_ride.driver_1.first_name} {this.props.active_ride.driver_1.last_name}
-                        </Row>
-                        <Row>
-                            Return Driver: {this.props.active_ride.driver_2.first_name} {this.props.active_ride.driver_2.last_name}
-                        </Row>
-                        <Row>
-                            Return Destination: {this.props.active_ride.locations.return}
-                        </Row>
-                    </Col>
-                </Row>
+            <Row>
+                <Col>
+                    <MapContainer>Trip Summary</MapContainer>
+                </Col>
+                <Col>
+                    <Row>Name: {`${this.props.active_ride.rider.first_name}`} {`${this.props.active_ride.rider.last_name}`}</Row>
+                    <Row>
+                        <Col>
+                            <Row>Pickup Location: {`${this.props.active_ride.locations.pickup.address}`}</Row>
+                            <Row>Dropoff Location: {`${this.props.active_ride.locations.dropoff.address}`}</Row>
+                            <Row>Return Location: {`${this.props.active_ride.locations.return.address}`}</Row>
+                        </Col>
+                        <Col>
+                            <Row>Pickup Time: {`${this.props.active_ride.locations.pickup.time}`}</Row>
+                            <Row>Dropoff Time: {`${this.props.active_ride.locations.pickup.time}`}</Row>
+                            <Row>Return Time: {`${this.props.active_ride.locations.pickup.time}`}</Row>
+                        </Col>
+                    </Row>
+                    <Row>Trip Duration: {`${this.props.active_ride.ride_data.time_total}`}</Row>
+                    <Row>Expected Traffic: {`${this.props.active_ride.ride_data.traffic}`}</Row>
+                    <Row>Driver: {`${this.props.active_ride.driver_1.first_name}`} {`${this.props.active_ride.driver_1.last_name}`}</Row>
+                    <Row>Return Driver: {`${this.props.active_ride.driver_1.first_name}`} {`${this.props.active_ride.driver_1.last_name}`}</Row>
+                </Col>
+            </Row>
             </Container>
-
         );
     }
 }
 
 const mapStateToProps = state => ({
-    active_ride: state.active_ride,
+    active_ride: state.active_ride
 });
 
 const mapDispatchToProps = dispatch => ({
