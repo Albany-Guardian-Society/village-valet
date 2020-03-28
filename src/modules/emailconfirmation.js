b17238d117d66f7366e63f417e9958f7c4f71e7e
Zapdos26 <zapdoskid@gmail.com>
2020-03-28 16:05:01 -0400

    Added backend email confirmation sender Updated packages

---
+++ src/modules/emailconfirmation.js
@@ -0,0 +1,44 @@
+const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
+
+const oauth2Client = new OAuth2(
+    process.env.REACT_APP_GMAIL_CLIENT_ID, // ClientID
+    process.env.REACT_APP_GMAIL_CLIENT_SECRET, // Client Secret
+    "https://developers.google.com/oauthplayground" // Redirect URL
+);
+oauth2Client.setCredentials({
+    refresh_token: process.env.REACT_APP_GMAIL_REFRESH_TOKEN
+});
+const accessToken = oauth2Client.getAccessToken();
+
+// async..await is not allowed in global scope, must use a wrapper
+const sendEmail = async(address, subject, text, html) => {
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            type: 'OAuth2',
+            user: process.env.REACT_APP_GMAIL_EMAIL,
+            clientId: process.env.REACT_APP_GMAIL_CLIENT_ID,
+            clientSecret: process.env.REACT_APP_GMAIL_CLIENT_SECRET,
+            refreshToken: process.env.REACT_APP_GMAIL_REFRESH_TOKEN,
+            accessToken:accessToken,
+        }
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+        from: `"Village Valet" <${process.env.REACT_APP_GMAIL_EMAIL}>`, // sender address
+        to: address, // list of receivers
+        subject, // Subject line
+        text, // plain text body
+        html // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+};
+
+export default sendEmail;
