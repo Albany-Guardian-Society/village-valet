95893ba8c6489e44c17702d8bbaad90cb85def6e
Matthew Menendez <45768592+mmenendez13@users.noreply.github.com>
2020-03-29 16:10:38 -0400

    updated email contents

--- src/components/scheduler/Confirmation.js
+++ src/components/scheduler/Confirmation.js
@@ -17,10 +17,39 @@ class Confirmation extends Component {
     handleChange(event){
     };
 
+    message = {
+        // Comma separated list of recipients
+        to: '${riderFirstName} ${riderLastName} <${riderEmail}>, ${driverFirstName} ${driverLastName} <${driverEmail}> ',
+
+        // Subject of the message
+        subject: 'AGS Village Valet Ride Confirmation' + Date.now(),
+
+        // plaintext body
+        text: '',
+
+        // HTML body
+        html: "<p><strong>AGS Village Valet Ride Confirmation</strong></p>\n" +
+            "<p><u><strong>Rider Trip Summary</strong></u></p>\n" +
+            "<p>Rider Name: {this.props.active_ride.rider.first_name} + \" \" + {this.props.active_ride.rider.last_name}</p>\n" +
+            "<p>Pickup Location: {this.props.active_ride.locations.pickup_address}</p>\n" +
+            "<p>Dropoff Location: {this.props.active_ride.locations.dropoff_address}</p>\n" +
+            "<p>Trip Duration: {this.props.active_ride.ride_data.time_total}</p>\n" +
+            "<p>Expected Traffic: {this.props.active_ride.ride_data.traffic}</p>\n" +
+            "<p>\n" +
+            "  <br>\n" +
+            "</p>\n" +
+            "<p><strong><u>Driver Trip Summary</u></strong></p>\n" +
+            "<p>Pickup Driver Name: {this.props.active_ride.driver_1.first_name} + ' ' + {this.props.active_ride.driver_1.last_name}&nbsp;</p>\n" +
+            "<p>Return Driver Name: {this.props.active_ride.driver_2.first_name} + ' ' + {this.props.active_ride.driver_2.last_name}</span>&nbsp;</p>\n" +
+            "<p>Return Destination: {this.props.active_ride.locations. return address}</p>",
+
+    };
+
     render() {
         return (
             <Container className="Confirmation" style={{minWidth: "100%"}}>
                 <h1>Confirmation</h1>
+                {this.message}
                 <Row>
                     <Col>
                         <MapContainer>Trip Summary</MapContainer>
--- src/modules/emailconfirmation.js
+++ src/modules/emailconfirmation.js
@@ -41,18 +41,3 @@ const sendEmail = async (message) => {
 export default sendEmail;
 
 
-// Message object
-let message = {
-    // Comma separated list of recipients
-    to: '${riderFirstName} ${riderLastName} <${riderEmail}>, ${driverFirstName} ${driverLastName} <${driverEmail}> ',
-
-    // Subject of the message
-    subject: 'AGS Village Valet Ride Confirmation' + Date.now(),
-
-    // plaintext body
-    text: 'Below is the information about the Scheduled Rider',
-
-    // HTML body
-    html: "<b>AGS Village Valet Ride Information</b>",
-
-};
