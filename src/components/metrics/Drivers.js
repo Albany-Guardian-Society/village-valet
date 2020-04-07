327917db87dd5e42cbd16be39d9ef689e5fa8abf
Nick Zoner <nick.zoner@scubeenterprise.com>
2020-04-07 12:46:19 -0400

    Fixed the bug

--- src/components/metrics/Drivers.js
+++ src/components/metrics/Drivers.js
@@ -2,43 +2,37 @@ import React, { Component } from 'react';
 import { connect } from "react-redux";
 import Table from "react-bootstrap/Table";
 
-
-
 class Drivers extends Component {
     constructor(props) {
         super(props);
         this.state = {
         };
-        this.renderTableData = this.handleChange.bind(this);
-        this.renderTableHeader = this.handleChange.bind(this);
         this.handleChange = this.handleChange.bind(this);
     }
 
     renderTableData() {
-        // let drivers = this.props.users.filter((user) => {
-        //         return(user.user_type === 'driver')
-        // });
-        // console.log(drivers);
-        // return drivers.map((driver) => {
-        //     // const { driverId, driver, numberRides, mileage, volunteerHours } = drivers; //destructuring
-        //
-        //     return (
-        //         <tr key={driver}>
-        //             <td>{driver.id}</td>
-        //             {/*<td>{driver}</td>*/}
-        //             {/*<td>{numberRides}</td>*/}
-        //             {/*<td>{mileage}</td>*/}
-        //             {/*<td>{volunteerHours}</td>*/}
-        //         </tr>
-        //     )
-        // })
+        let drivers = this.props.users.filter((user) => {
+                return(user.user_type === 'driver')
+        });
+        console.log(drivers);
+        return drivers.map((driver) => {
+            return (
+                <tr key={driver.id}>
+                    <td>{driver.id}</td>
+                    {/*<td>{driver}</td>*/}
+                    {/*<td>{numberRides}</td>*/}
+                    {/*<td>{mileage}</td>*/}
+                    {/*<td>{volunteerHours}</td>*/}
+                </tr>
+            )
+        })
     }
 
     renderTableHeader() {
         console.log('here');
         let header = ['Driver Id', 'Driver', 'Rides', 'Mileage', 'Volunteer Hours'];
         return header.map((item) => {
-            return <th> {item} </th>
+            return <th key={item}>{item}</th>
         })
     }
 
@@ -48,15 +42,17 @@ class Drivers extends Component {
     render() {
         return (
             <div>
-                <h1 id='title'>Drivers123</h1>
-                <table id='driverId'>
+                <h1 id='title'>Drivers</h1>
+                <Table>
                     <thead>
-                    <tr>{this.renderTableHeader()}</tr>
+                        <tr>
+                        {this.renderTableHeader()}
+                        </tr>
                     </thead>
                     <tbody>
                         {this.renderTableData()}
                     </tbody>
-                </table>
+                </Table>
             </div>
         );
     }
