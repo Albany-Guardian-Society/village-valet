5be8cc32a5e95b397ed7d41c06a57fc4bdf83695
Matthew Menendez <45768592+mmenendez13@users.noreply.github.com>
2020-04-10 14:04:55 -0400

    Updated Rider Email
    the blank fields will be filled in once the driver object is added to props

--- src/components/scheduler/Confirmation.js
+++ src/components/scheduler/Confirmation.js
@@ -20,10 +20,10 @@ class Confirmation extends Component {
     handleChange(event){
     };
 
-    message = {
+    riderMessage = {
         // Comma separated list of recipients
-        to: '"' + this.props.users[this.props.active_ride.driver_1.id].personal_info.first_name + " " +this.props.users[this.props.active_ride.driver_1.id].personal_info.last_name + '" <' + this.props.users[this.props.active_ride.driver_1.id].personal_info.email + ">, " +
-            '"' + this.props.users[this.props.active_ride.rider.id].personal_info.first_name + " " +this.props.users[this.props.active_ride.rider.id].personal_info.last_name + '" <' + this.props.users[this.props.active_ride.rider.id].personal_info.email + ">",
+        to: '"' + this.props.users[this.props.active_ride.rider.id].personal_info.first_name + " " +this.props.users[this.props.active_ride.rider.id].personal_info.last_name +
+            '" <' + this.props.users[this.props.active_ride.rider.id].personal_info.email + ">",
 
         // Subject of the message
         subject: 'AGS Village Valet Ride Confirmation: ' + this.props.active_ride.ride_data.date,
@@ -32,29 +32,38 @@ class Confirmation extends Component {
         text: '',
 
         // HTML body
-        html: `<p><strong>AGS Village Valet Ride Confirmation</strong></p>\n` +
-            `<p><u><strong>Rider Trip Summary</strong></u></p>\n` +
-            `<p>Rider Name: ${this.props.active_ride.rider.first_name}  ${this.props.active_ride.rider.last_name}</p>\n` +
-            `<p>Pickup Address: ${this.props.active_ride.locations.pickup.address}` +
-            `<p>Pickup Time: ${this.props.active_ride.locations.pickup.time}</p>\n` +
-            `<p>Dropoff Address: ${this.props.active_ride.locations.dropoff.address}</p>\n` +
-            `<p>Dropoff Time: ` +
-            `<p>Trip Duration: ${this.props.active_ride.ride_data.time_total}</p>\n` +
-            `<p>Expected Traffic: ${this.props.active_ride.ride_data.traffic}</p>\n` +
-            `<p>\n` +
-            `  <br>\n` +
-            `</p>\n` +
-            `<p><strong><u>Driver Trip Summary</u></strong></p>\n` +
-            `<p>Pickup Driver Name: ${this.props.active_ride.driver_1.first_name} ${this.props.active_ride.driver_1.last_name}&nbsp;</p>\n` +
-            `<p>Return Driver Name: ${this.props.active_ride.driver_2.first_name} ${this.props.active_ride.driver_2.last_name}</span>&nbsp;</p>\n` +
-            `<p>Return Address: ${this.props.active_ride.locations.return.address}` +
-            `<p>Return Time: ${this.props.active_ride.locations.return.time}</p>`,
+        html: `<p><strong><u>AGS Village Valet Ride Confirmation</u></strong></p>\n` +
+            `<p>Hello ${this.props.users[this.props.active_ride.rider.id].personal_info.first_name} ${this.props.users[this.props.active_ride.rider.id].personal_info.last_name},</p>\n` +
+            `<p>The following information is your trip summary for your scheduled ride.</p>\n` +
+            `<p><strong>Date:</strong> ${this.props.active_ride.ride_data.date} </p>\n` +
+            `<p><strong>Pickup Address:</strong> ${this.props.active_ride.locations.pickup.address}</p>\n` +
+            `<p><strong>Pickup Time:</strong> ${this.props.active_ride.locations.pickup.time}</p>\n` +
+            `<p><strong>Drop off Address:</strong> ${this.props.active_ride.locations.dropoff.address}</p>\n` +
+            `<p><strong>Drop off Time: TBD</strong></p>\n`+
+            `<p><strong>Expected Traffic: TBD</strong></p>\n` +
+            `<p><strong>Total Trip Duration: TBD</strong></p>\n` +
+            `<p><br></p>\n` +
+            `<p>Your driver, ${this.props.users[this.props.active_ride.driver_1.id].personal_info.first_name} ${this.props.users[this.props.active_ride.driver_1.id].personal_info.last_name}
+            , will be driving a ${this.props.users[this.props.active_ride.driver_1.id].vehicles[0].color} ${this.props.users[this.props.active_ride.driver_1.id].vehicles[0].make_model} with
+             the license plate ${this.props.users[this.props.active_ride.driver_1.id].vehicles[0].lp}. 
+            They are aware of any special accommodations that you may have requested: ${this.props.users[this.props.active_ride.rider.id].accommodations.special}. You will be unable to cancel this 
+            ride 48 hours prior to the pick up time. If you have any questions or would like to make any changes please feel 
+            free to contact us.</p>\n` +
+            `<p><br></p>\n` +
+            `<p>Sincerely,</p>\n` +
+            `<p>Village Valet</p>\n` +
+            `<p><br></p>\n` +
+            `<p>Village Valet Phone Number</p>\n` +
+            `<p>Village Valet Address</p>\n` +
+            `<p>Village Valet Email</p>\n`
 
     };
 
+
+
     messageTest() {
-        console.log(this.message);
-        axios.post('http://localhost:4000/send', {...this.message}).then()
+        console.log(this.riderMessage);
+        axios.post('http://localhost:4000/send', {...this.riderMessage}).then()
     }
 
     render() {
