ae8a464689b273f55a7f577644d8e86648c58b83
Zapdos26 <zapdoskid@gmail.com>
2020-04-10 13:41:16 -0400

    Commiting changes

--- src/components/google-maps/MapContainer.js
+++ src/components/google-maps/MapContainer.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {DirectionsRenderer, DirectionsService, GoogleMap, LoadScript} from '@react-google-maps/api';
+import * as moment from 'moment';
 
 
 class MapContainer extends Component {
@@ -8,16 +9,17 @@ class MapContainer extends Component {
         this.state = {
             directions_cache: new Map()
         };
-		this.handleChange = this.handleChange.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.locations = {};
     }
 
-	handleChange(event) {
-	}
+    handleChange(event) {
+    }
 
-    directionsCallback(response, ride_id) {
-        if (response !== null) {
+    directionsCallback(response, rideId) {
+        if (response !== null && rideId != null) {
             if (response.status === 'OK') {
-                this.state.directions_cache.set(ride_id, response);
+                this.state.directions_cache.set(rideId, response);
                 this.setState({directions_cache: this.state.directions_cache});
             } else {
                 console.log('response: ', response)
@@ -25,22 +27,44 @@ class MapContainer extends Component {
         }
     }
 
+    locationOrder() {
+        if (this.props.ride.driver.dropoff == null || this.props.ride.driver.pickup == null) return;
+        if (this.props.ride.driver) {
+            this.locations['origin'] = this.props.ride.driver.current_geolocation;
+            if (this.props.ride.driver.current_geolocation === this.props.ride.locations.pickup.geolocation) {
+                this.locations['destination'] = this.props.ride.driver.home_geolocation;
+                this.locations['waypoint'] = this.props.ride.driver.dropoff.geolocation;
+            } else {
+                this.locations['destination'] = this.props.ride.driver.dropoff.geolocation;
+                this.locations['waypoint'] = this.props.ride.driver.pickup.geolocation;
+            }
+        } else {
+            this.locations['origin'] = this.props.ride.pickup.geolocation;
+            this.locations['destination'] = this.props.ride.driver.dropoff.geolocation;
+        }
+    }
 
-    makeDirections(ride_id, locations) {
-        if (locations === undefined || ride_id === undefined) return;
-        if (this.state.directions_cache.has(ride_id)) {
+    makeDirections(rideId) {
+        if (this.state.directions_cache.has(rideId)) {
             return;
         }
         return <DirectionsService
             options={{
-                origin: locations.origin,
-                destination: locations.destination,
+                origin: this.props.ride.driver.geolocation ? this.props.ride.driver.geolocation : this.props.ride.locations.pickup.geolocation,
+                destination: this.props.ride.locations.dropoff.geolocation,
                 travelMode: 'DRIVING',
-                waypoints: locations.pickup == null ? [] : [{location: locations.pickup}],
-                optimizeWaypoints: true
+                waypoints: this.props.ride.driver.geolocation ? [{
+                    location: this.props.ride.locations.pickup.geolocation,
+                    stopover: false
+                }],
+                optimizeWaypoints: false,
+                drivingOptions: {
+                    departureTime: moment(this.props.ride.ride_data.date + ' ' + this.props.ride.locations.pickup.time),
+                    trafficModel: 'pessimistic'
+                },
             }}
             callback={resp => {
-                this.directionsCallback(resp, ride_id);
+                this.directionsCallback(resp, rideId);
             }}
             onLoad={directionsService => {
                 console.log('DirectionsService onLoad directionsService: ', directionsService)
@@ -51,9 +75,9 @@ class MapContainer extends Component {
         />;
     }
 
-    renderDirections(ride_id) {
-        if (ride_id === undefined) return;
-        const response = this.state.directions_cache.get(ride_id);
+    renderDirections(rideId) {
+        if (rideId === undefined) return;
+        const response = this.state.directions_cache.get(rideId);
         if (response == null) return;
         return <DirectionsRenderer
             options={{
@@ -79,10 +103,10 @@ class MapContainer extends Component {
                     id='map'
                     zoom={8}
                     mapContainerStyle={{width: "100%", height: "100%"}}
-                    center={{ lat: 42.6526, lng: -73.7562}}
+                    center={{lat: 42.6526, lng: -73.7562}}
                 >
-                    {this.props.ride && this.makeDirections(this.props.ride.id, this.props.ride.locations)}
-                    {this.props.ride && this.renderDirections(this.props.ride.id)}
+                    {this.props.ride && this.makeDirections(this.props.ride.ride_id || this.props.ride.rider.id)}
+                    {this.props.ride && this.renderDirections(this.props.ride.ride_id || this.props.ride.rider.id)}
                 </GoogleMap>
             </LoadScript>
         )
