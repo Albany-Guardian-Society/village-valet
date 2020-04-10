4a2d671de4f33f8fbe8c8114a0a48778d31e3c10
Schmaj <jwschmalz@outlook.com>
2020-04-10 13:51:25 -0400

    Updating Ledger Page

--- src/components/ledger/Ledger.js
+++ src/components/ledger/Ledger.js
@@ -5,6 +5,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
 const TEST = [
     {
@@ -43,6 +44,13 @@ class Ledger extends Component {
             <Container style={{minWidth: "100%"}}>
                 <Row>
                     <Col sm={4}><Form.Control type="search" id="search" placeholder="Search" onChange={this.handleChange}/></Col>
+                    <Col sm={1}/>
+                    <Col sm={5}/>
+                    <Col sm={2}>
+                        <Button id="cancel_button" variant="danger">
+                            Cancel Ride
+                        </Button>
+                    </Col>
                 </Row>
                 <hr/>
                 <Row>
