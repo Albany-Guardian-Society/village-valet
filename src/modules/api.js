975f92aefe2cae275d45c3f92d898d16917c3806
Zapdos26 <zapdoskid@gmail.com>
2020-04-30 15:51:46 -0400

    Fixed api.js

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,9 +1,10 @@
 const apiVersion = 'v1';
 let hostname = window && window.location && window.location.hostname;
+export let API_ROOT;
 if (hostname === 'localhost') {
     hostname += ':3000'
-    export const API_ROOT = `http://${hostname}/api/${apiVersion}`;
+    API_ROOT = `http://${hostname}/api/${apiVersion}`;
 } else {
-    export const API_ROOT = `https://${hostname}/api/${apiVersion}`;
+    API_ROOT = `https://${hostname}/api/${apiVersion}`;
 }
 
