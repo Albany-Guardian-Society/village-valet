c13e25ecc71b5bbd9b05af88486158b716fbcd71
Zapdos26 <zapdoskid@gmail.com>
2020-04-30 15:30:03 -0400

    Special case if localhost

--- src/modules/api.js
+++ src/modules/api.js
@@ -2,7 +2,8 @@ const apiVersion = 'v1';
 let hostname = window && window.location && window.location.hostname;
 if (hostname === 'localhost') {
     hostname += ':3000'
+    export const API_ROOT = `http://${hostname}/api/${apiVersion}`;
+} else {
+    export const API_ROOT = `https://${hostname}/api/${apiVersion}`;
 }
-
-export const API_ROOT = `https://${hostname}/api/${apiVersion}`;
 
